bb78d3621a023d30eae744f470c059413f64a186
Louis Rubet <lrubet@gmail.com>
2024-01-24 18:31:27 +0100

    debug assert

--- quickjs-fake/quickjs.h
+++ quickjs-fake/quickjs.h
@@ -2,7 +2,7 @@
 #pragma once
 
 #include <assert.h>
-static void QJS_ASSERT(int something) { assert(something); }
-static void QJS_ABORT() { assert(0); }
+#define QJS_ASSERT(something) { if (!(something)) printf("%s:%d - %s: assert\n", __FILE__, __LINE__, __FUNCTION__); assert(something); }
+#define QJS_ABORT() { printf("%s:%d - %s: abort\n", __FILE__, __LINE__, __FUNCTION__); assert(0); }
 static int IsDebuggerPresent() { return 0; }
 static void DebugBreak() { }
